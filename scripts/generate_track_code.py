7d127891b546fe90d8a4f2bac521f196c362ba8f
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-24 15:13:37 +0200

    properly normalize func name

--- scripts/generate_track_code.py
+++ scripts/generate_track_code.py
@@ -15,7 +15,12 @@ def {fname}():
 
 """
 
+def normalize_funcname(fname):
+	for c in ['/', '<', ':', '>', ',', ' ']:
+		fname = fname.replace(c, '_')
+	return fname
+
 with open(os.path.join(gen_dir, "track_code_gen.py"), "w") as fo:
 	for b in benchmarks["benchmarks"]:
-		fname = "track_" + b['name'].replace('/', '_')
+		fname = "track_" + normalize_funcname(b['name'])
 		fo.write(temp.format(fname=fname, result=b['real_time']))
