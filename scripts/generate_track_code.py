468b09ca41b69f846367378997e9e6c0eeb34ce3
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-24 18:46:09 +0200

    fix cpp import of benchmarks

--- scripts/generate_track_code.py
+++ scripts/generate_track_code.py
@@ -9,10 +9,21 @@ gen_dir = sys.argv[2]
 with open(result_file, "r") as fi:
 	benchmarks = json.load(fi)
 
+header =  f"""
+import json
+with open('{result_file}', 'r') as fi:
+	j_dict = json.load(fi)
+
+def extract_result(result_name):
+	for b in j_dict['benchmarks']:
+		if b['name'] == result_name:
+			return b['real_time']
+	return 0
+"""
+
 temp = """
 def {fname}():
-	return {result}
-
+	return extract_result('{result_name}')
 """
 
 def normalize_funcname(fname):
@@ -21,6 +32,8 @@ def normalize_funcname(fname):
 	return fname
 
 with open(os.path.join(gen_dir, "track_code_gen.py"), "w") as fo:
+	fo.write(header)
+
 	for b in benchmarks["benchmarks"]:
 		fname = "track_" + normalize_funcname(b['name'])
-		fo.write(temp.format(fname=fname, result=b['real_time']))
+		fo.write(temp.format(fname=fname, result_name=b['name']))
